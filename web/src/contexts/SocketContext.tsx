c599e7306f70a311ae15fadecfaa37e6f8154749
nguyentuan-2001 <tuan2k1tvtb@gmail.com>
2023-07-16 00:21:23 +0700

    thai-fishnal

--- web/src/contexts/SocketContext.tsx
+++ web/src/contexts/SocketContext.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode, createContext, useEffect, useState } from 'react';
 import socketIOClient, { Socket } from "socket.io-client";
 
 interface ISocketContext {
+    socketClient: Socket | null
 }
 
 interface PropsSocketContext {
@@ -12,26 +13,26 @@ export const SocketContext = createContext<ISocketContext | undefined>(undefined
 
 const SocketProvider: React.FC<PropsSocketContext> = ({ children }) => {
 
-    // const socket = socketIOClient(
-    //     process.env.SERVER_WEB_SOCKET || "http://localhost:7000/da2"
-    // );
-    const socket =''
-
+    const [socketClient, setSocketClient] = useState<Socket | null>(null);
 
     //join Room
-    // useEffect(() => {
+    useEffect(() => {
+        const socket = socketIOClient(
+            process.env.SERVER_WEB_SOCKET || "http://192.168.0.105:5005/da2"
+        );
+        setSocketClient(socket)
 
-    //     const payload = { userId: 1 };
-    //     socket.emit("joinMap", payload);
+        const payload = { userId: 1 };
+        socket.emit("joinUser", payload);
 
-    //     //note return
-    //     return () => {
-    //         socket.disconnect();
-    //     };
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, []);
+        //note return
+        return () => {
+            socket.disconnect();
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-    const data = {  }
+    const data = { socketClient }
     return (
         <SocketContext.Provider value={data}>
             {children}
--- web/src/layout/MapReal.tsx
+++ web/src/layout/MapReal.tsx
@@ -1,27 +1,81 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import maplibregl, { LngLatLike, Map } from "maplibre-gl";
 import '../css/map.css'
 import distance from '@turf/distance';
 import { point } from "@turf/helpers";
-import { CoordinateAPI } from "../api/coordinate";
-import { useLocation } from "react-router";
 import MapComponent from "../components/MapComponent";
+import { SocketContext } from "../contexts/SocketContext";
+import { CloseCircleOutlined,MessageOutlined } from '@ant-design/icons';
+
+interface Data {
+  created: string;
+  heartRate: number;
+}
 
 const MapReal= () => {
+  const socketContext = useContext(SocketContext)
+  const socketClient = socketContext?.socketClient
 
-  function realTime(map: Map){
+  const [dataReal, setDataReal] = useState<any[ ]>([])
+  const [duration, setDuration] = useState<number | undefined>()
+  const [heartRates, setHeartRate] = useState<number | undefined>()
+  const [lengthStreet, setLengthStreet] = useState<string | undefined>()
+  const [legStreet, setLegStreet] = useState<string | undefined>()
+
+  const [isClosed, setIsClosed] = useState(true);
+
+  useEffect(() => {
+    if (socketClient) {
+      socketClient.on("start", (data) => {
+        setDataReal([])
+      })
+
+      socketClient.on("end", (data) => {
+        setDataReal([])
+      })
+      
+      socketClient.on("data", (data) => {
+        console.log(data);
+        setDataReal((pre) => [...pre, [...data.geometry.coordinates]])
+        setLegStreet(data.properties.step)
+        setHeartRate(data.properties.heartRate)
+        setDuration(data.properties.duration)
+      })
+
+      return () => {
+        socketClient.off("start")
+        socketClient.off("end")
+        socketClient.off("data")
+      }
+    }
+  }, [socketClient])
+
+  function realTime(map: Map ){
     map.on("load", function () {
-      let coordinates = [
-        [1,2],
-        [2,3],
-        [3,4],
-      ];
-
+      realTimes(dataReal)
+    });
+    
+    
+  function realTimes(coordinates: any[]){
+      if (!coordinates?.length) {
+        return;
+      }
       const start = coordinates[0];
       const end = coordinates[coordinates.length - 1]
+      const startp = point(start);
+      const finish = point(end);
+      const calculatedDistance = distance(startp, finish, { units: 'kilometers' });
+      const calculatedDistanceInMeters = calculatedDistance * 1000;
+      setLengthStreet(calculatedDistanceInMeters.toFixed(2));
 
+      if(heartRates){
+        const heartRate =  heartRates.toFixed(0);
+        const parsedheartRate = parseFloat(heartRate);
+        setHeartRate(parsedheartRate);
+      }
+    
       const checkStart = map.getSource("source-real");
-      //map.setCenter(start);
+      map.setCenter(start);
       if (checkStart) {
         (checkStart as maplibregl.GeoJSONSource).setData({
           type: "Feature",
@@ -101,12 +155,34 @@ const MapReal= () => {
           }
         });
       }
-    });
   }
-
+}
+  console.log(isClosed);
+  
   return (
     <div>
-      <MapComponent />
+      {
+        isClosed ?<MessageOutlined id="icon-close"  onClick={() => setIsClosed(!isClosed) } />:
+        <CloseCircleOutlined id="icon-close"  onClick={() => setIsClosed(!isClosed)} /> 
+      }
+      <div id='if-length' style={{ display: isClosed ? 'none' : 'block' }}>
+        <div id="start">
+          <b></b>
+          <p>Điểm bắt đầu</p>
+        </div>
+        <div id="end">
+          <b></b>
+          <p>Điểm kết thúc</p>
+        </div>
+        <p id='length_street'>Quãng đường di chuyển: {lengthStreet} m</p>
+        <p id='time_street'>Thời gian di chuyển: {} phút</p>
+        <p id='heart'>Nhịp tim: {heartRates} nhịp/phút</p>
+        <p id='leg'>Số bước chân: {legStreet} bước</p>
+      </div>
+      <MapComponent 
+        realTime={realTime}
+        dataCoordinates={dataReal}
+      />
     </div>
   )
 }
